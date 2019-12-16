77faa5e005fb26143442c5a0c903212f1288f4ee
mjhead <andremjhead@gmail.com>
2019-12-16 17:33:28 +0200

    FIX: max-width: 100% for inputs

--- cherry-x-vue-ui.php
+++ cherry-x-vue-ui.php
@@ -2,7 +2,7 @@
 /**
  * Vue.js based Interface Builder module
  *
- * Version: 1.0.3
+ * Version: 1.0.4
  */
 
 // If this file is called directly, abort.
@@ -42,7 +42,7 @@ if ( ! class_exists( 'CX_Vue_UI' ) ) {
 		 *
 		 * @var string
 		 */
-		protected $version = '1.0.3';
+		protected $version = '1.0.4';
 
 		protected $assets_enqueued = false;
 
