f27c9a302996d5f5594f0845378610bc37cc3d25
shinTM <emasugeta@gmail.com>
2020-03-03 15:23:55 +0200

    upd: dimensions styles

--- cherry-x-vue-ui.php
+++ cherry-x-vue-ui.php
@@ -2,7 +2,7 @@
 /**
  * Vue.js based Interface Builder module
  *
- * Version: 1.0.6
+ * Version: 1.0.7
  */
 
 // If this file is called directly, abort.
@@ -42,8 +42,12 @@ if ( ! class_exists( 'CX_Vue_UI' ) ) {
 		 *
 		 * @var string
 		 */
-		protected $version = '1.0.6';
+		protected $version = '1.0.7';
 
+		/**
+		 * [$assets_enqueued description]
+		 * @var boolean
+		 */
 		protected $assets_enqueued = false;
 
 		/**
