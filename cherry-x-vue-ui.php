d0851310dd2bdb4e85241700d9f07d543db2b422
shinTM <emasugeta@gmail.com>
2020-11-16 15:56:07 +0200

    fix: f-select placeholder

--- cherry-x-vue-ui.php
+++ cherry-x-vue-ui.php
@@ -2,7 +2,7 @@
 /**
  * Vue.js based Interface Builder module
  *
- * Version: 1.0.8
+ * Version: 1.0.9
  */
 
 // If this file is called directly, abort.
@@ -42,7 +42,7 @@ if ( ! class_exists( 'CX_Vue_UI' ) ) {
 		 *
 		 * @var string
 		 */
-		protected $version = '1.0.8';
+		protected $version = '1.0.9';
 
 		/**
 		 * [$assets_enqueued description]
