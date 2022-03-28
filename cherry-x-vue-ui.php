63b1c557aca2e3d46db926d57af92e010ad95859
Dmitriy Bartoshchak <kaskad.8888@gmail.com>
2022-03-28 14:55:00 +0300

    UPD: bump version to 1.4.1

--- cherry-x-vue-ui.php
+++ cherry-x-vue-ui.php
@@ -2,7 +2,7 @@
 /**
  * Vue.js based Interface Builder module
  *
- * Version: 1.4.0
+ * Version: 1.4.1
  */
 
 // If this file is called directly, abort.
@@ -42,7 +42,7 @@ if ( ! class_exists( 'CX_Vue_UI' ) ) {
 		 *
 		 * @var string
 		 */
-		protected $version = '1.4.0';
+		protected $version = '1.4.1';
 
 		/**
 		 * [$assets_enqueued description]
